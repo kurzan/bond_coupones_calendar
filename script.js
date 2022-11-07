ced11124644d0efeac900eae3860462035e95fac
Kurzov <kurzanbl4@gmail.com>
2022-11-07 22:10:52 +0300

    Новый вариант

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const table = document.querySelector('.coupone-table');
+const tableBody = document.querySelector('.coupone-table__body');
 const bondTitle = document.querySelector('.bond_title');
+const bondTextCoupons = document.querySelector('.bond_text__coupons');
 
 
 function dateToRus(date) {
@@ -8,48 +9,98 @@ function dateToRus(date) {
     month: 'long',
     day: 'numeric',
   });
-
 }
 
 const today = new Date();
 
-console.log(typeof(today))
 
-function couponList(item, index) {
-  const newElement = document.createElement('tr');
+function newCouponCalendar(item) {
+  const calendar = document.createElement('tr');
   const date = new Date(item.coupondate);
-  newElement.classList.add('coupone-table__list');
-  newElement.innerHTML = `
-      <th>${index}</th>
+  calendar.classList.add('coupone-table__item');
+  calendar.innerHTML = `
       <th>${dateToRus(date)}</th>
       <th>${item.value_rub}₽</th>
       <th>${item.valueprc}%</th>
 	`;
 
-  if (date < today) {
-    newElement.querySelectorAll('th').forEach((el) => {
-      el.classList.add('old-coupones');
-    });
-  }
-
-  
-  
-  return newElement;
+  return calendar;
 }
 
-function renderCalendar(data) {
-  const coupones = data[1].coupons;
-  console.log(coupones);
-  coupones.forEach((element, i) => {
-    let index = i + 1;
-    bondTitle.textContent = element.name;
-    table.append(couponList(element, index));
-  });
+function oldCouponCalendar(item) {
+  const calendar = document.createElement('tr');
+  const date = new Date(item.coupondate);
+  calendar.classList.add('coupone-table__item');
+  calendar.classList.add('old-coupones');
+  calendar.innerHTML = `
+      <th>${dateToRus(date)}</th>
+      <th>${item.value_rub}₽</th>
+      <th>${item.valueprc}%</th>
+	`;
+
+  return calendar;
 }
 
+function currentCouponCalendar(item) {
+  const calendar = document.createElement('tr');
+  const date = new Date(item.coupondate);
+  calendar.classList.add('coupone-table__item');
+  calendar.classList.add('feature-coupones');
+  calendar.innerHTML = `
+      <th>${dateToRus(date)}</th>
+      <th>${item.value_rub}₽</th>
+      <th>${item.valueprc}%</th>
+	`;
 
-fetch('https://iss.moex.com/iss/securities/RU000A102LF6/bondization.json?iss.json=extended&iss.meta=off&iss.only=coupons&lang=ru&limit=unlimited')
+  return calendar;
+}
+
+// function renderCalendar(data) {
+//   const coupones = data[1].coupons;
+//   console.log(coupones);
+//   coupones.forEach((element, i) => {
+//     let index = i + 1;
+//     bondTitle.textContent = element.name;
+//     tableBody.append(couponList(element));
+//   });
+// }
+
+
+
+fetch('https://iss.moex.com/iss/securities/RU000A0ZZAT8/bondization.json?iss.json=extended&iss.meta=off&iss.only=coupons&lang=ru&limit=unlimited')
   .then(r => r.json())
-  .then((data) => renderCalendar(data));
+  .then((data) => {
+    let coupons = data[1].coupons;
+    const allCoupones = coupons.length;
+
+    const oldCoupons = [];
+    const featureCoupons = [];
+
+    coupons.forEach((el) => {
+      const date = new Date(el.coupondate);
+      if (date < today) {
+        oldCoupons.push(el);
+      } else {
+        featureCoupons.push(el);
+      }
+    })
+    
+    
+    oldCoupons.forEach((el) => {
+      tableBody.append(oldCouponCalendar(el));
+    });
+
+    const currentCoupon = featureCoupons[0];
+    tableBody.append(currentCouponCalendar(currentCoupon));
+
+    featureCoupons.forEach((el) => {
+      tableBody.append(newCouponCalendar(el));
+    });
+
+    const paysCoupons = oldCoupons.length;
+
+
+    bondTextCoupons.textContent = `Выплачено купонов: ${paysCoupons} из ${allCoupones}`;
+  });
 
 
