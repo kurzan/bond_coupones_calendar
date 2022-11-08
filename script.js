56a759dc34112e203d812ce67e90fe7c780c001a
Kurzov <kurzanbl4@gmail.com>
2022-11-08 15:21:37 +0300

    реализация с использованием двух массивов

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+const calendarWindget = document.querySelector('.coupone-calendar-widget');
 const tableBody = document.querySelector('.coupone-table__body');
 const bondTitle = document.querySelector('.bond_title');
 const bondTextCoupons = document.querySelector('.bond_text__coupons');
+const showAllCouponsButton = document.querySelector('.show-all-coupons');
+
+const isin = 'RU000A0ZZAT8';
+const api_url = `https://iss.moex.com/iss/securities/${isin}/bondization.json?iss.json=extended&iss.meta=off&iss.only=coug=ru&limit=unlimited`;
+
+const today = new Date();
+let visible_old_coupons = 3;
+let visible_feauters_coupons = 8;
 
 
 function dateToRus(date) {
@@ -11,15 +20,11 @@ function dateToRus(date) {
   });
 }
 
-const today = new Date();
-
-
 function newCouponCalendar(item) {
   const calendar = document.createElement('tr');
-  const date = new Date(item.coupondate);
   calendar.classList.add('coupone-table__item');
   calendar.innerHTML = `
-      <th>${dateToRus(date)}</th>
+      <th>${dateToRus(item.coupondate)}</th>
       <th>${item.value_rub}₽</th>
       <th>${item.valueprc}%</th>
 	`;
@@ -29,11 +34,10 @@ function newCouponCalendar(item) {
 
 function oldCouponCalendar(item) {
   const calendar = document.createElement('tr');
-  const date = new Date(item.coupondate);
   calendar.classList.add('coupone-table__item');
   calendar.classList.add('old-coupones');
   calendar.innerHTML = `
-      <th>${dateToRus(date)}</th>
+      <th>${dateToRus(item.coupondate)}</th>
       <th>${item.value_rub}₽</th>
       <th>${item.valueprc}%</th>
 	`;
@@ -43,34 +47,47 @@ function oldCouponCalendar(item) {
 
 function currentCouponCalendar(item) {
   const calendar = document.createElement('tr');
-  const date = new Date(item.coupondate);
   calendar.classList.add('coupone-table__item');
   calendar.classList.add('feature-coupones');
   calendar.innerHTML = `
-      <th>${dateToRus(date)}</th>
+      <th>${dateToRus(item.coupondate)}</br><span class="current_coupon_span">Ближайшая выплата<span></th>
       <th>${item.value_rub}₽</th>
       <th>${item.valueprc}%</th>
+      
 	`;
 
   return calendar;
 }
 
-// function renderCalendar(data) {
-//   const coupones = data[1].coupons;
-//   console.log(coupones);
-//   coupones.forEach((element, i) => {
-//     let index = i + 1;
-//     bondTitle.textContent = element.name;
-//     tableBody.append(couponList(element));
-//   });
-// }
+
+function renderCalendar(oldCoupons, newCoupons) {
+  oldCoupons.forEach((el) => {
+    tableBody.append(oldCouponCalendar(el));
+  });
+
+  const currentCoupon = newCoupons[0];
+  tableBody.append(currentCouponCalendar(currentCoupon));
+
+  for (let i = 1; i < newCoupons.length; i++) {
+    tableBody.append(newCouponCalendar(newCoupons[i]));
+  }
+}
+
+function removeWidget() {
+  calendarWindget.remove();
+}
 
 
-
-fetch('https://iss.moex.com/iss/securities/RU000A0ZZAT8/bondization.json?iss.json=extended&iss.meta=off&iss.only=coupons&lang=ru&limit=unlimited')
+fetch(api_url)
   .then(r => r.json())
   .then((data) => {
-    let coupons = data[1].coupons;
+    const coupons = data[1].coupons;
+    const bondName = coupons[0].name
+
+    bondTitle.textContent = bondName;
+    
+    console.log(coupons)
+    //Кол-во купонов у облигации
     const allCoupones = coupons.length;
 
     const oldCoupons = [];
@@ -84,23 +101,25 @@ fetch('https://iss.moex.com/iss/securities/RU000A0ZZAT8/bondization.json?iss.jso
         featureCoupons.push(el);
       }
     })
-    
-    
-    oldCoupons.forEach((el) => {
-      tableBody.append(oldCouponCalendar(el));
-    });
 
-    const currentCoupon = featureCoupons[0];
-    tableBody.append(currentCouponCalendar(currentCoupon));
-
-    featureCoupons.forEach((el) => {
-      tableBody.append(newCouponCalendar(el));
-    });
-
+    // Кол-во выплаченных купонов
     const paysCoupons = oldCoupons.length;
+    
+    const filteredOLdCoupons = oldCoupons.slice(-visible_old_coupons);
+    const filteredFeautersCoupons = featureCoupons.slice(0, visible_feauters_coupons);
 
-
+    renderCalendar(filteredOLdCoupons, filteredFeautersCoupons);
+    
     bondTextCoupons.textContent = `Выплачено купонов: ${paysCoupons} из ${allCoupones}`;
-  });
 
+    showAllCouponsButton.addEventListener('click', () => {
+      tableBody.innerHTML = '';
+      renderCalendar(oldCoupons, featureCoupons);
 
+      showAllCouponsButton.remove();
+    })
+  })
+  .catch(() => {
+    removeWidget();
+  })
+  ;
